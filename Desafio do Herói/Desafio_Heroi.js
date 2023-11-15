c6919d9d39c49086abf8dad1337e341c070c18ca
Letícia Reis <leticiareis017@gmail.com>
2023-11-15 16:34:05 -0300

    Create Desafio_Heroi.js
    Resolução do desafio 'Classificador de nível de Herói'

---
+++ Desafio do Herói/Desafio_Heroi.js
@@ -0,0 +1,28 @@
+
+let nomeHeroi = "Fellipe"
+let nivelXp = 8000
+
+if (nivelXp <= 1000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
+
+} else if (nivelXp >= 1001 && nivelXp <= 2000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
+
+}else if (nivelXp >= 2001 && nivelXp <= 5000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
+
+}else if (nivelXp >= 5001 && nivelXp <= 7000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
+
+}else if (nivelXp >= 7001 && nivelXp <= 8000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
+
+}else if (nivelXp >= 8001 && nivelXp <= 9000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
+
+}else if (nivelXp >= 9001 && nivelXp <= 10000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal")
+
+}else if (nivelXp >= 10001 ){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante")
+}
